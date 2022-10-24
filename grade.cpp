3269eadbe44d76de77ceec08ff3759df77cfe61a
Isaiah Peralta <iperalta@uri.edu>
2022-10-24 19:36:16 -0400

    updating member functions

--- grade.cpp
+++ grade.cpp
@@ -6,12 +6,36 @@
 
 double Grade::calculate_grade(){
     
-};
+}
+
+double Grade::review_proj(){
+    int g;
+    std::cin >> g >> std::endl;
+    g = g * (30/10);
+    return g;
+}
+
+double Grade::lab_grade(int g){
+    
+    
+}
+
+double Grade::assignment_grade(int g){
+     numAssignment = 500; 
+     g = g * (); 
+     
+    
+}
+    
+
+
 
 
 
 double Grade::term_proj(){
-    double term_proj_grade;
-    
-    return term_proj_grade;
+    double tproj_grade;
+    std::cin >> tproj_grade;
+
+    tproj_grade = tproj_grade/
+    return tproj_grade;
 };
